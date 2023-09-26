b0c500db72bf0b742ac2ab025dd0c48885dabc76
Cat1tac <waynepowell235@gmail.com>
2023-09-25 20:53:15 -0400

    Laid out card designs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './style.css';
 
+const newTask = document.querySelector(".addNewTask");
+const form = document.querySelector("div.formContainer");
+
+newTask.addEventListener('click', ()=> {
+    if (form.classList.contains("notVisible")){
+        form.classList.remove('notVisible');
+        form.classList.add('visiblie');
+        console.log(form.getElementsByClassName('visible'));
+    } else {
+        form.classList.remove('visible');
+        form.classList.add('notVisible');
+    }
+});
